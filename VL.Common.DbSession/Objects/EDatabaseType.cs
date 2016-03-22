53058c026796e6d1e0b8293bd90a4b2329f560c1
vlong638 <vlong638@163.com>
2016-03-22 17:44:23 +0800

    实现DbSession

---
+++ VL.Common.DbSession/Objects/EDatabaseType.cs
@@ -0,0 +1,13 @@
+﻿namespace VL.Common.DbSession.Objects
+{
+    /// <summary>
+    /// 数据库类别
+    /// </summary>
+    public enum EDatabaseType
+    {
+        None,
+        Oracle,
+        MSSQL,
+        MySQL,
+    }
+}
