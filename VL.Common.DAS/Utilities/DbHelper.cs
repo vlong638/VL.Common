b5f961f1b2db3592fcad94d575eb8499ac9f1774
vlong638 <vlong638@163.com>
2016-04-06 15:02:57 +0800

    VL.Common.DAS 调整DbHelper以支持DbConfigHelper

--- VL.Common.DAS/Utilities/DbHelper.cs
+++ VL.Common.DAS/Utilities/DbHelper.cs
@@ -4,9 +4,13 @@ namespace VL.Common.DAS.Utilities
 {
     public class DbHelper
     {
-        public static DbSession GetDbSession(EDatabaseType dbType, string dbName)
+        public static DbSession GetDbSession(string dbName)
         {
-            return new DbSession(dbType, DbConfigHelper.GetDbConnectingString(dbName));
+            return new DbSession(DbConfigHelper.GetDbType(dbName), DbConfigHelper.GetDbConnectingString(dbName));
+        }
+        public static void CreateConfigFile()
+        {
+            DbConfigHelper.CreateConfigFile();
         }
     }
 }
