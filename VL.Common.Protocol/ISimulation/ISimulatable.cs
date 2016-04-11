b5b1bb9da0e92a88303ef29f7852b0ca5d7355f2
vlong638 <vlong638@163.com>
2016-04-11 09:06:03 +0800

    Protocol增加ISimulation

---
+++ VL.Common.Protocol/ISimulation/ISimulatable.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace VL.Common.Protocol.ISimulation
+{
+    public interface ISimulatable<T>
+    {
+        bool SimulateCreate(T t, DateTime simulateTime);
+    }
+}
--- VL.Common.Protocol/Properties/AssemblyInfo.cs
+++ VL.Common.Protocol/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("VL.Common.Protocol")]
-[assembly: AssemblyDescription("增加IResult规范(接口的返回码)")]
+[assembly: AssemblyDescription("VL生态圈协议")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VL.Common.Protocol")]
