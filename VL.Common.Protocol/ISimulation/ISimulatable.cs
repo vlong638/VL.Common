d55b695ae5f40480ac3058a6616ac4085800fc79
vlong638 <vlong638@163.com>
2016-04-11 10:24:10 +0800

    移除ReferenceSample依赖
    ISimulatable接口调整(确定模拟的三要素,空间,时间,对象)

--- VL.Common.Protocol/ISimulation/ISimulatable.cs
+++ VL.Common.Protocol/ISimulation/ISimulatable.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using VL.Common.DAS.Objects;
+using VL.Common.Protocol.IResult;
 
 namespace VL.Common.Protocol.ISimulation
 {
+    /// <summary>
+    /// 模拟三要素
+    /// 空间:模拟落实的物理位置
+    /// 对象:模拟的目标内容
+    /// 时间:时间总是在变化的,我们总是试图去模拟某一个时间点的行为.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface ISimulatable<T>
     {
-        bool SimulateCreate(T t, DateTime simulateTime);
+        Result SimulateCreate(DbSession session, T t, DateTime simulateTime);
     }
 }
