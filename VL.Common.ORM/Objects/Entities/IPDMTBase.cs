fd60a838b1204f2b681071ce4443b24464484017
vlong638 <vlong638@163.com>
2016-04-12 15:09:00 +0800

    IPDMTBase支持DataContract

--- VL.Common.ORM/Objects/Entities/IPDMTBase.cs
+++ VL.Common.ORM/Objects/Entities/IPDMTBase.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Data;
+using System.Runtime.Serialization;
 
 namespace VL.Common.ORM.Objects
 {
@@ -9,11 +10,19 @@ namespace VL.Common.ORM.Objects
     /// 需有TableName,Properties
     /// 同时他定义了基于会话DbSession的操作基本规范
     /// </summary>
+    [DataContract]
     public abstract class IPDMTBase
     {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
         public IPDMTBase()
         {
         }
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="reader"></param>
         public IPDMTBase(IDataReader reader)
         {
             Init(reader);
@@ -27,7 +36,6 @@ namespace VL.Common.ORM.Objects
         /// <summary>
         /// 通过DataReader初始化数据
         /// </summary>
-        /// <param name="reader"></param>
         public abstract void Init(IDataReader reader, List<string> fields);
         /// <summary>
         /// 获取表名
