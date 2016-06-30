c1d3110d337fe168c1e4605d1e2c38eabfbdc85f
vlong638 <vlong638@163.com>
2016-06-30 15:39:13 +0800

    细节修正

--- VL.Common.Protocol/IService/IDependency/DependencyResult.cs
+++ VL.Common.Protocol/IService/IDependency/DependencyResult.cs
@@ -45,11 +45,11 @@ namespace VL.Common.Protocol.IService
 
         public void UpdateInfoFromDependencies()
         {
+            IsAllDependenciesAvailable = DependencyDetails.Where(c => c.IsDependencyAvailable != true).Count() == 0
+                && DependencyResults.Where(c => c.IsAllDependenciesAvailable != true).Count() == 0;
             Message = "单元<" + UnitName + ">" + (IsAllDependenciesAvailable ? "依赖检测通过" : "依赖检测未通过,错误详情:" + System.Environment.NewLine
-                + string.Join(System.Environment.NewLine, DependencyDetails.Where(c => c.IsDependencyAvailable == false).Select(c => c.Message ))
+                + string.Join(System.Environment.NewLine, DependencyDetails.Where(c => c.IsDependencyAvailable == false).Select(c => c.Message))
                 + string.Join(System.Environment.NewLine, DependencyResults.Where(c => c.IsAllDependenciesAvailable == false).Select(c => string.Format("依赖单元:{1}{0}错误详情:{2}{0}", System.Environment.NewLine, c.UnitName, c.Message))));
-            IsAllDependenciesAvailable= DependencyDetails.Where(c => c.IsDependencyAvailable != true).Count() == 0
-               && DependencyResults.Where(c => c.IsAllDependenciesAvailable != true).Count() == 0;
         }
     }
 }
