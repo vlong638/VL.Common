32cddfc70ec08bfbfed0ad5df0eb6f31723213af
vlong638 <vlong638@163.com>
2017-05-27 14:26:59 +0800

    Log4net Configuration

--- VL.Logger/LogData.cs
+++ VL.Logger/LogData.cs
@@ -5,8 +5,28 @@ namespace VL.Logger
 {
     public class LogData
     {
+        public LogData(string message)
+        {
+            Message = message;
+        }
+        public LogData(string className, string fuctionName, string message)
+        {
+            ClassName = className;
+            FuctionName = fuctionName;
+            Message = message;
+        }
+
+        public LogData(string className, string fuctionName, string segregation, string message)
+        {
+            ClassName = className;
+            FuctionName = fuctionName;
+            Segregation = segregation;
+            Message = message;
+        }
+
         public string ClassName { set; get; }
         public string FuctionName { set; get; }
+        public string Segregation { set; get; }
         public string Message { set; get; }
 
         public override string ToString()
@@ -15,6 +35,7 @@ namespace VL.Logger
             sb.AppendLine("----------------------------");
             sb.AppendLine(string.Format("-------ClassName  :{0}", ClassName));
             sb.AppendLine(string.Format("-------FuctionName:{0}", FuctionName));
+            sb.AppendLine(string.Format("-------Segregation:{0}", Segregation));
             sb.AppendLine(string.Format("-------LogTime    :{0}", DateTime.Now));
             sb.AppendLine(string.Format("-------Message    :{0}", Message));
             return sb.ToString();
